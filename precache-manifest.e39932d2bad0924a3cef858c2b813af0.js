38556a5849f9b7e18766c7d57540a1687c2f564f
mito343 <webdazer0@gmail.com>
2020-09-24 23:06:34 +0200

    Updates

---
+++ precache-manifest.e39932d2bad0924a3cef858c2b813af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d254776cffc77f0b20bc05dc4eb5f2",
+    "url": "/yankenpo-react/index.html"
+  },
+  {
+    "revision": "731cd7669f96a4ee0cec",
+    "url": "/yankenpo-react/static/css/main.574371fb.chunk.css"
+  },
+  {
+    "revision": "17e7c476e94af1e512e3",
+    "url": "/yankenpo-react/static/js/2.7a56dbb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yankenpo-react/static/js/2.7a56dbb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731cd7669f96a4ee0cec",
+    "url": "/yankenpo-react/static/js/main.67a21159.chunk.js"
+  },
+  {
+    "revision": "64060c1d46daa00fa48f",
+    "url": "/yankenpo-react/static/js/runtime-main.31e57695.js"
+  }
+]);
